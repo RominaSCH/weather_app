79445f1ea26eebe4efc9619daacb7d582b8f62b8
Romina Schann <cuqi213@gmail.com>
2021-03-25 00:03:47 +0900

    weather v0.1

---
+++ Loading.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { SnapshotViewIOS, StyleSheet, Text, View } from 'react-native';
+
+export default function Loading(){
+    return <View style={styles.container}>
+        <Text style={styles.text}>Getting the Weather</Text>
+    </View>
+}
+
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:"#0063B2",
+        justifyContent:"flex-end",
+        alignItems:"center",
+        paddingHorizontal:30,
+        paddingVertical:100,
+    },
+    text:{
+        fontSize:30,
+        color:"white",
+    }
+    
+  });
